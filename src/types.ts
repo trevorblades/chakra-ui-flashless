df56c69e6e3bcc78931512da28d45f9ec3a8baba
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-28 17:49:31 -0300

    Add ColorModeToggleProps type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,9 @@
+import {Dict} from '@chakra-ui/utils';
+import {PropsWithChildren} from 'react';
+
 export type Color = string | [string, number];
 export type Variables = Record<string, [Color, Color]>;
+export type ColorModeToggleProps = PropsWithChildren<{
+  theme: Dict;
+  customVariables: Variables;
+}>;
