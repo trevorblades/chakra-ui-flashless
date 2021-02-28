8e5f18cfea98e210f8d81456f81437022f3a8055
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-02-28 17:40:23 -0300

    Add file for constants

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const BASE_VARIABLES = {
+  '--bg': ['white', 'gray.800'],
+  '--text': ['gray.800', 'whiteAlpha.900'],
+  '--placeholder-text': ['gray.400', 'whiteAlpha.400'],
+  '--border': ['gray.200', 'whiteAlpha.300'],
+  '--badge-text': ['white', 'whiteAlpha.800']
+};
